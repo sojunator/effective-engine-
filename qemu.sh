db0242c33fd284a3efe2495064baddb6223c59c3
Carl Söyseth <carl@MacBook-Air-som-tillhor-Carl.local>
2024-06-27 13:13:34 +0200

    Launch qemu with -kernel flag to make running kernel faster

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,5 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel isodir/boot/myos.kernel
